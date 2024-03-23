118b8100c63b71ad4b27f3035cf722ab47d3fc19
Elzombiegame <128931595+Elzombiegame@users.noreply.github.com>
2024-03-22 21:12:43 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,7 +4,19 @@ const gojoVivo = document.getElementById("gojoVivo");
 const gojoMuerto = document.getElementById("gojoMuerto");
 const win = document.getElementById("win");
 const gameplay = document.getElementById("gameplay");
+const satoru = document.getElementById("satoru");
+const menu = document.getElementById("menu");
+const musica = document.getElementById("musica");
+const fondo = document.getElementById("fondo");
 let i= 100;
+let valor;
+
+
+function jugar(){
+    menu.style= "display: none;"
+    gameplay.style = "display:block;"
+    musica.play();
+}
 function gojomuerto(){
     gojoVivo.style= "display: none;"
     gojoMuerto.style= "display: block;"
@@ -12,7 +24,10 @@ function gojomuerto(){
 }
 function contadorVida() {
     if(i <=1){
-        vida.style = "    width: "+0+"%;";     
+        vida.style = "    width: "+0+"%;";   
+        fondo.style = "background-image: url(img/fondo.jpg);background-repeat: no-repeat; background-size: cover;"  ;
+        musica.pause();
+
         setTimeout(() => {
             gojomuerto();
         }, 16000);
@@ -23,8 +38,21 @@ function contadorVida() {
             win.style = "display: block;"
         }, 18000);
     }else{
-        //i= i-50;
+      // i= i-50;
         i--;
         vida.style = "    width: "+i+"%;";
+    }
+}
+
+function animacion(){
+    satoru.style= "animation: none;"
+    if (valor === "izquieda"){
+        satoru.style= "animation: movimientoIzquieda 0.5s linear;"
+        valor = "derecha";
+
+    }else{
+        satoru.style= "animation: movimientoDerecha 0.5s linear;"
+        valor = "izquieda";
+
     }
 }
