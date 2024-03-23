67d2ab16f6767db9c21fb9c9db675ac80af3682c
Elzombiegame <128931595+Elzombiegame@users.noreply.github.com>
2024-03-23 16:49:28 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const menu = document.getElementById("menu");
 const musica = document.getElementById("musica");
 const fondo = document.getElementById("fondo");
 let i= 100;
+const tijeras = document.getElementById("tijeras");
 let valor;
 
 
@@ -22,23 +23,35 @@ function gojomuerto(){
     gojoMuerto.style= "display: block;"
 
 }
+function corte(){
+    tijeras.style = "display:flex;";
+
+
+}
 function contadorVida() {
     if(i <=1){
         vida.style = "    width: "+0+"%;";   
         fondo.style = "background-image: url(img/fondo.jpg);background-repeat: no-repeat; background-size: cover;"  ;
         musica.pause();
-
+        audio.play();
         setTimeout(() => {
+            tijeras.style = "display:none;";
             gojomuerto();
         }, 16000);
-        audio.play();
-        vida.style = "    width: "+0+"%;";     
+        setTimeout(() => {
+            corte();
+
+        }, 10000);
+
+        
+
+           
         setTimeout(() => {
             gameplay.style = "display: none;"
             win.style = "display: block;"
         }, 18000);
     }else{
-      // i= i-50;
+        // i= i-50;
         i--;
         vida.style = "    width: "+i+"%;";
     }
