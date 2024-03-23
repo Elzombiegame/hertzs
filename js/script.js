283b41fe331532bcc2ed006568b2f6f13fa7b408
Elzombiegame <128931595+Elzombiegame@users.noreply.github.com>
2024-03-22 18:08:44 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 const vida = document.getElementById("vida");
+const audio = document.getElementById("audio");
+const gojoVivo = document.getElementById("gojoVivo");
+const gojoMuerto = document.getElementById("gojoMuerto");
+const win = document.getElementById("win");
+const gameplay = document.getElementById("gameplay");
 let i= 100;
-function contadorVida() {
-    if(i <=0){
+function gojomuerto(){
+    gojoVivo.style= "display: none;"
+    gojoMuerto.style= "display: block;"
 
+}
+function contadorVida() {
+    if(i <=1){
+        vida.style = "    width: "+0+"%;";     
+        setTimeout(() => {
+            gojomuerto();
+        }, 16000);
+        audio.play();
+        vida.style = "    width: "+0+"%;";     
+        setTimeout(() => {
+            gameplay.style = "display: none;"
+            win.style = "display: block;"
+        }, 18000);
     }else{
+        i= i-50;
         i--;
         vida.style = "    width: "+i+"%;";
     }
-    
 }
